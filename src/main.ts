691e75e63f5a65102c457b8140a5056bb8bb95ac
ohhyunsok <terecal@daum.net>
2024-07-19 20:34:01 +0900

    update for url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 여러 주소를 포함하는 CORS 설정
-  const allowedOrigins = ['http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'https://dankkumi-hyunsok-ohs-projects.vercel.app'];
+  const allowedOrigins = ['http://127.0.0.1:3000', 'http://52.79.208.201:3000/', 'https://dankkumi-hyunsok-ohs-projects.vercel.app'];
 
   app.enableCors({
     origin: allowedOrigins, // 여러 주소를 배열로 추가
