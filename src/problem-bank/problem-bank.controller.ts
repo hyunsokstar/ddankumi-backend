7662dda329bf1e5380e5b6c981506c28502ccc23
ohhyunsok <terecal@daum.net>
2024-08-05 17:30:47 +0900

    update for swagger

--- src/problem-bank/problem-bank.controller.ts
+++ src/problem-bank/problem-bank.controller.ts
@@ -4,10 +4,36 @@ import { CreateExamDto } from './dto/create-exam.dto';
 import { UpdateExamDto } from './dto/update-exam.dto';
 import { CreateProblemDto } from './dto/create-problem-dto';
 import { CreateAnswerOptionDto } from './dto/create-answer-option.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('exam') // Swagger 태그 추가
 @Controller('problem-bank')
 export class ProblemBankController {
   constructor(private readonly problemBankService: ProblemBankService) { }
+
+  @Get('exam')
+  @ApiOperation({ summary: '모든 시험 조회' }) // 메서드 설명 추가
+  @ApiResponse({ status: 200, description: '성공적으로 모든 시험을 조회했습니다.' })
+  findAllExam() {
+    // exam 모델에 대해 모든 리스트 가져 오는 서비스 함수 호출
+    return this.problemBankService.getAllExamList();
+  }
+
+  @Get('exam-only/')
+  findAllExamOnly() {
+    // exam 모델에 대해 모든 리스트 가져 오는 서비스 함수 호출
+    return this.problemBankService.getAllExamListOnly();
+  }
+
+  @Post('exam')
+  @ApiOperation({ summary: 'Create an exam' }) // 메서드 설명 추가
+  @ApiResponse({ status: 201, description: 'The exam has been successfully created.' }) // 응답 설명 추가
+  @ApiResponse({ status: 403, description: 'Forbidden.' }) // 다른 상태 코드에 대한 설명 추가
+  @ApiBody({ type: CreateExamDto }) // 요청 바디 설명 추가
+  create(@Body() createExamDto: CreateExamDto) {
+    // exam 모델에 대해 생성 서비스 함수 호출
+    return this.problemBankService.createOneExam(createExamDto);
+  }
 
   // 시험 삭제
   @Delete('exam/:id')
@@ -29,20 +55,6 @@ export class ProblemBankController {
   @Get('exam/:examId/problem')
   findAllProblemByExamId(@Param('examId', ParseIntPipe) examId: number) {
     return this.problemBankService.getAllProblemForExamId(examId);
-  }
-
-  // exam list
-  @Get('exam')
-  findAllExam() {
-    // exam 모델에 대해 모든 리스트 가져 오는 서비스 함수 호출
-    return this.problemBankService.getAllExamList();
-  }
-
-  // exam list
-  @Get('exam-only/')
-  findAllExamOnly() {
-    // exam 모델에 대해 모든 리스트 가져 오는 서비스 함수 호출
-    return this.problemBankService.getAllExamListOnly();
   }
 
   // 특정 문제에 대해 옵션 5개 한번에 추가
@@ -80,13 +92,6 @@ export class ProblemBankController {
     @Body() createProblemDto: CreateProblemDto
   ) {
     return this.problemBankService.createProblem(examId, createProblemDto);
-  }
-
-  // exam 추가
-  @Post('exam')
-  create(@Body() createExamDto: CreateExamDto) {
-    // exam 모델에 대해 생성 서비스 함수 호출
-    return this.problemBankService.createOneExam(createExamDto);
   }
 
   // exam 추가
